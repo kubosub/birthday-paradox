9c01009828ff982c714e5a6f8b192abe90ce4a30
Jakob Svanholm <jakob@sub.se>
2022-09-12 15:05:57 +0200

    better input validation

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 '''This is a simulation of the birthday paradox (the birthday problem).'''
-from sys import exit
 from random import randint
 
 def get_birthdays(size):
@@ -14,31 +13,31 @@ def check_match(bdays):
     '''Check if birthday list equals length of unique-birthdays list (set)'''
     return len(bdays) != len(set(bdays))
 
-def validate_number(num):
-    '''Is it a valid number?'''
-    return num.isdigit() and int(num) > 0
+def input_valid_integer(prompt):
+    '''Validate integer input'''
+    while True:
+        user_input = input(prompt)
+        try:
+            user_input = int(user_input)
+            if (user_input < 1): raise Exception("Sorry, no numbers below zero")
+            return user_input
+        except:
+            print("Does not compute. Try again.")
 
 def main():
     '''Main program'''
-    groups = input("Number of groups: ")
-    group_size = input("Number of students in each group: ")
-
-    if not (validate_number(groups) and validate_number(group_size)):
-        print("INPUT ERROR")
-        exit()
-
-    groups_number = int(groups)
-    group_size_number = int(group_size)
+    groups = input_valid_integer("Number of groups: ")
+    group_size = input_valid_integer("Number of students in each group: ")
 
     match_count = 0
-    for _ in range(groups_number):
-        bdays = get_birthdays(group_size_number)
+    for _ in range(groups):
+        bdays = get_birthdays(group_size)
         if check_match(bdays):
             match_count += 1
 
-    print(f"Of {groups_number} groups with {group_size_number} people in each group, "\
+    print(f"Of {groups} groups with {group_size} people in each group, "\
           f"we found that {match_count} groups had matching birthdays.\n"\
-          f"Thats {match_count/groups_number*100}% of the groups.")
+          f"Thats {match_count/groups*100}% of the groups.")
 
 if __name__ == '__main__':
     main()
